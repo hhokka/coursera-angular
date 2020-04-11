f4c6f45b5055cbd0164ed8fc52dc8c1933b490ff
Hans Hokka <hans.hokka@gmail.com>
2020-04-11 09:55:46 +0300

    Assignment 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,15 +9,22 @@ import { AppComponent } from "./app.component";
 import "hammerjs";
 import { MenuComponent } from "./menu/menu.component";
 import { MatListModule } from "@angular/material/list";
+import { MatGridListModule } from "@angular/material/grid-list";
+import { MatCardModule } from "@angular/material/card";
+import { MatButtonModule } from "@angular/material/button";
+import { DishdetailComponent } from './dishdetail/dishdetail.component';
 
 @NgModule({
-  declarations: [AppComponent, MenuComponent],
+  declarations: [AppComponent, MenuComponent, DishdetailComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     FlexLayoutModule,
     MatToolbarModule,
     MatListModule,
+    MatGridListModule,
+    MatCardModule,
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
