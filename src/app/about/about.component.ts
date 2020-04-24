6c7f73ee744969b5a6a7af773706f07d07d52c4b
Hans Hokka <hans.hokka@gmail.com>
2020-04-24 13:50:57 +0300

    Animations Part 2

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -3,11 +3,18 @@ import { Leader } from "../shared/leader";
 import { LeaderService } from "../services/leader.service";
 import { Params, ActivatedRoute } from "@angular/router";
 import { Location } from "@angular/common";
+import { flyInOut, expand } from "../animations/app.animation";
 
 @Component({
   selector: "app-about",
   templateUrl: "./about.component.html",
   styleUrls: ["./about.component.scss"],
+  // tslint:disable-next-line:use-host-property-decorator
+  host: {
+    "[@flyInOut]": "true",
+    style: "display: block;",
+  },
+  animations: [flyInOut()],
 })
 export class AboutComponent implements OnInit {
   leaders: Leader[];
--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -7,37 +7,18 @@ import { switchMap } from "rxjs/operators";
 import { ViewChild } from "@angular/core";
 import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 import { Feedback, ContactType } from "../shared/feedback";
-import {
-  trigger,
-  state,
-  style,
-  animate,
-  transition,
-} from "@angular/animations";
+import { visibility } from "../animations/app.animation";
+import { flyInOut, expand } from "../animations/app.animation";
 
 @Component({
   selector: "app-dishdetail",
   templateUrl: "./dishdetail.component.html",
   styleUrls: ["./dishdetail.component.scss"],
-  animations: [
-    trigger("visibility", [
-      state(
-        "shown",
-        style({
-          transform: "scale(1.0)",
-          opacity: 1,
-        })
-      ),
-      state(
-        "hidden",
-        style({
-          transform: "scale(0.5)",
-          opacity: 0,
-        })
-      ),
-      transition("* => *", animate("0.5s ease-in-out")),
-    ]),
-  ],
+  host: {
+    "@flyInOut": "true",
+    style: "display: block",
+  },
+  animations: [visibility(), expand()],
 })
 export class DishdetailComponent implements OnInit {
   dish: Dish;
